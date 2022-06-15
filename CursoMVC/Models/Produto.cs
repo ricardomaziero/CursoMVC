4cf946854dbed0b5329b75bf826f04987a8ee166
ricardo <ricardomaziero87@gmail.com>
2022-06-15 09:13:37 -0300

    Add project files.

---
+++ CursoMVC/Models/Produto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CursoMVC.Models
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Descrição")]
+        [Required(ErrorMessage = "Campo obrigatório")]
+        public string Descricao { get; set; }
+
+        [Range(1, 100, ErrorMessage = "Quantidade não permitida")]
+        public int Quantidade { get; set; }
+        public int CategoriaId { get; set; }
+        public Categoria Categoria { get; set; }
+    }
+}
