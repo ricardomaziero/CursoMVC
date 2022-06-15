3f44f2ec96b7ca86e2e7e576a0e7a9085fbdcf33
ricardo <ricardomaziero87@gmail.com>
2022-06-15 10:41:50 -0300

    AulaDIO

--- CursoMVC/Models/Categoria.cs
+++ CursoMVC/Models/Categoria.cs
@@ -13,6 +13,6 @@ namespace CursoMVC.Models
         [Display(Name = "Descrição")]
         [Required(ErrorMessage = "O campo descrição é obrigatório")]
         public string Descricao { get; set; }
-        public List<Produto> Produtos { get; set; }
+        
     }
 }
